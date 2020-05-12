01743cfb4a786c04e3c0cc1097a87fa0f59b7503
L7z404 <leonelcastillo1014@gmail.com>
2020-05-11 21:21:25 -0500

    -Form cliente completo

--- javascript/valida_clientes.js
+++ javascript/valida_clientes.js
@@ -1,37 +1,18 @@
 function valida_clientes() {
-    var edad = document.getElementById("edad").value;
-    var nombre = document.getElementById("nombre").value;
-    var ap = document.getElementById("ap").value;
-    var am = document.getElementById("am").value;
+    var domicilio = document.getElementById("domicilio").value;
+    var rfc = document.getElementById("rfc").value;
+    var razonSocial = document.getElementById("razonSocial").value;
 
+    if (domicilio.length===0 && rfc.length === 0 && razonSocial.length === 0){
+        alert("Por favor llene el formulario")
+    } else if (domicilio.length < 5 || domicilio.length===0){
+        alert("Ingresa una direccion valida")
+    } else if (razonSocial.length < 3 || razonSocial.length===0){
+        alert("Inngresa una razon social valida")
+    } else if(rfc.length !== 13){
+        alert("Ingrese una rfc valida")
+    } else{
+        document.form_cliente.submit();
+    }
 
-    if (edad<18){
-        if (edad<=0){
-            alert("Por favor eliga una edad valida")
-        }else {
-            alert("Debes ser mayor de 17 años para llenar este formulario")
-        }
-    } else if (!document.querySelector('input[name="sexo"]:checked')){
-        alert("Por favor elige un genero")
-    }else if(document.forms["form_vendedor"]["fecha_contrato"].value === "" || document.forms["form_vendedor"]["fecha_contrato"].value === "1970-01-01"){
-        alert("Por favor eliga una fecha")
-    }
-    else if (nombre_valido(nombre,ap,am)!==false){
-        document.form_vendedor.submit();
-    }
-}
-
-function nombre_valido(nombre,ap,am) {
-    if (nombre.length < 3 || ap.length < 3 || am.length < 3){
-        if (nombre.length === 0 || ap.length === 0 || am.length === 0){
-            alert("Por favor ingrese los datos de su nombre")
-            return false;
-        } else{
-            alert("Por favor ingrese nombres completos")
-            return false;
-        }
-    }
-    else{
-        return true;
-    }
 }
